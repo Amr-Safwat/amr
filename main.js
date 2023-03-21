d2b2af3546cfee684e82eb04c159b83759a2f0ff
Amr Safwat <amrsafwat02@gmail.com>
2023-03-22 00:55:52 +0200

    show animation when scroll

--- main.js
+++ main.js
@@ -4,3 +4,15 @@ let menu = document.querySelector('.mega-menu');
 link.onclick = () => {
   menu.classList.toggle('show');
 }
+// Skills Section
+let skillsSection = document.querySelector('.our-skills');
+let progs = document.querySelectorAll('.progress-bar');
+
+window.onscroll = function () {
+  if(window.scrollY >= skillsSection.offsetTop) {
+    progs.forEach((prog)=>{
+      prog.style.width = prog.dataset.width;
+    })
+  }
+}
+// Skills Section
